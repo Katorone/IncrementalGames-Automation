793c2fc104eee983e3ac71fcd64731a7eee744f4
Kato <katorone@gmail.com>
2018-06-28 01:58:38 +0200

    Update SpaceCompanyBot.js

--- SpaceCompany/SpaceCompanyBot.js
+++ SpaceCompany/SpaceCompanyBot.js
@@ -1,4 +1,5 @@
 // Placeholder
 var Bot = {};
 Bot.version = "1.0 alpha-release";
-console.log("Bot "+Bot.version+" Loaded.");
+window.onload = function () { console.log("Space Company Bot -version "+Bot.version+"- Loaded."); }
+
