6a1c856ee0d9e1dcaf86aa74865577636d90d2ad
Kato <katorone@gmail.com>
2018-06-28 02:14:08 +0200

    Update SpaceCompanyBot.js

--- SpaceCompany/SpaceCompanyBot.js
+++ SpaceCompany/SpaceCompanyBot.js
@@ -1,13 +1,14 @@
 // Placeholder
 var Bot = {};
 Bot.version = "1.0 alpha-release";
+Bot.init = function() {
+     console.log("Space Company Bot -version "+Bot.version+"- Loaded.");
+}
 
-// Everything but IE
-window.addEventListener("load", function() {
-    console.log("Space Company Bot -version "+Bot.version+"- Loaded.");
-}, false); 
-
-// IE
-window.attachEvent("onload", function() {
-    // loaded
-});
+if (window.addEventListener) {
+    window.addEventListener("load", Bot.init, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", Bot.init);
+} else {
+    window.onload = handlerName;
+}
